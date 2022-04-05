9a83458a2cd1fad80af924615da2d1368df8e8e5
gp-sato <99707520+gp-sato@users.noreply.github.com>
2022-04-05 16:39:02 +0900

    about.blade.phpの作成

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <h1>このシステムについて</h1>
+            <p>このシステムはGiftPlaceのクリエーターの勤怠管理を行うものです。<br />
+            このシステムはGiftPlaceのクリエーターによって開発されました。</p>
+        </div>
+    </div>
+</div>
+@endsection
