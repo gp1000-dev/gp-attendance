3aefe9b4cba17efdd59ec91419ac854cd3766f32
gp-sato <99707520+gp-sato@users.noreply.github.com>
2022-04-05 15:24:21 +0900

    E-mailを'メール'に変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">E-mailアドレス</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">メールアドレス</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
