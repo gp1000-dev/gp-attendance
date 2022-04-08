2f1ee67b86e942689dbe6107f6738607eea612f8
gp-sato <99707520+gp-sato@users.noreply.github.com>
2022-04-08 17:09:04 +0900

    ユーザ－画面_index.blade.phpの作成

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <h1>ユーザー画面</h1>
+        </div>
+    </div>
+</div>
+@endsection
