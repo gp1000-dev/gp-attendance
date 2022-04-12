58b4dd942c203e997d43418c517c5470939bdd06
gp-nakai <102277988+gp-nakai@users.noreply.github.com>
2022-04-12 14:43:14 +0900

    修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
-                
+                </a>
                 <a href= "{{url('/about')}}" class="nav-link "  rel="noopener noreferrer" >
                     このシステムについて</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
