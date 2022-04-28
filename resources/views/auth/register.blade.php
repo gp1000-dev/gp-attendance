daf975ef6517d878cc8736f2e307c2c66233108b
gp-sato <99707520+gp-sato@users.noreply.github.com>
2022-04-28 16:44:17 +0900

    新規登録ページの項目の追加_register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,16 +12,26 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                            <label for="last_name" class="col-md-4 col-form-label text-md-end">氏名</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            <div class="col-md-2">
+                                <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+                            </div>
+                            &ensp;
+                            <div class="col-md-2">
+                                <input id="first_name" type="text" class="form-control" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name">
+                            </div>
+                        </div>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="row mb-3">
+                            <label for="last_kana_name" class="col-md-4 col-form-label text-md-end">氏名カナ</label>
+
+                            <div class="col-md-2">
+                                <input id="last_kana_name" type="text" class="form-control" name="last_kana_name" value="{{ old('last_kana_name') }}" required autocomplete="last_kana_name">
+                            </div>
+                            &ensp;
+                            <div class="col-md-2">
+                                <input id="first_kana_name" type="text" class="form-control" name="first_kana_name" value="{{ old('first_kana_name') }}" required autocomplete="first_kana_name">
                             </div>
                         </div>
 
@@ -58,6 +68,54 @@
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label class="col-md-4 col-form-label text-md-end">性別</label>
+
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input id="male" type="radio" class="form-check-input" name="gender" value="male" checked>
+                                    <label class="form-check-label" for="male">男性</label>
+                                </div>
+                                <div class="form-check">
+                                    <input id="female" type="radio" class="form-check-input" name="gender" value="female">
+                                    <label class="form-check-label" for="female">女性</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label class="col-md-4 col-form-label text-md-end">誕生日</label>
+
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <select name="birthdate_year" class="form-select">
+                                            @foreach (range(\Carbon\Carbon::now()->addYears(-60)->year, \Carbon\Carbon::now()->year) as $year)
+                                                <option value="{{ $year }}" {{ $year === old('birthdate_year') ? 'selected' : '' }}>{{ $year }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    年
+                                    <div class="col-md-3">
+                                        <select name="birthdate-month" class="form-select">
+                                            @foreach (range(1, 12) as $month)
+                                                <option value="{{ $month }}" {{ $month === old('birthdate_month') ? 'selected' : '' }}>{{ $month }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    月
+                                    <div class="col-md-3">
+                                        <select name="birthdate-day" class="form-select">
+                                            @foreach (range(1, 31) as $day)
+                                                <option value="{{ $day }}" {{ $day === old('birthdate_day') ? 'selected' : '' }}>{{ $day }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    日
+                                </div>
                             </div>
                         </div>
 
