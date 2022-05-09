2907a3f3ba15b0082571f31da4fb51df3eca083d
gp-yamada <yamada@gp1000.jp>
2022-05-09 17:03:33 +0900

    勤怠テストデータ

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Attendance;
 use App\Models\User;
 use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
 class AttendanceSeeder extends Seeder
 {
@@ -17,15 +18,22 @@ class AttendanceSeeder extends Seeder
     public function run()
     {
         $user = User::first();
+        $thisMonth = Carbon::today()->startOfMonth();
 
-        Attendance::factory()
-            ->for($user)
-            ->create([
-                'date' => Carbon::parse('2022-04-01'),
-                'attended' => true,
-                'start_time' => Carbon::parse('13:00'),
-                'end_time' => Carbon::parse('18:00'),
-                'comment' => '通常出所',
-            ]);
+        // 3ヶ月前〜1日前 の勤怠データ作成
+        collect(CarbonPeriod::create($thisMonth->clone()->addMonth(-3), Carbon::today()->addDay(-1))->toArray())
+            ->map(function ($day) use ($user) {
+                $isWorkDay = !in_array($day->dayOfWeek, [0, 3, 6]);
+                $attended = $isWorkDay && (rand(0, 4) < 4) || !$isWorkDay && (rand(0, 4) === 0);
+                Attendance::factory()
+                    ->for($user)
+                    ->create([
+                        'date' => $day,
+                        'attended' => $attended,
+                        'start_time' => $attended ? Carbon::parse('13:00') : null,
+                        'end_time' => $attended ? Carbon::parse('18:00') : null,
+                        'comment' => $isWorkDay && !$attended ? '休暇' : (!$isWorkDay && $attended ? '休日出勤' : null),
+                    ]);
+            });
     }
 }
