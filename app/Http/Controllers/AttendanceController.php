187d5087b402086910503f69c347cbf1b9f4dfbf
gp-sato <99707520+gp-sato@users.noreply.github.com>
2022-05-17 14:23:36 +0900

    createメソッドの追加_AttendanceController.php

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -29,4 +29,31 @@ class AttendanceController extends Controller
 
         return view('attendances/index')->with('dt', $dt);
     }
+
+    /**
+     * show attendance create page.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function create(Request $request)
+    {
+        $query = null; /* クエリがなければヌルになる */
+        $query = $request->date; /* ?date=のみ受け付ける */
+
+        /* 正規表現で年月日の形式か判定する */
+        if (preg_match('/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/', $query)) {
+            /* 日付型に変換する */
+            $date = explode("-", $query);
+            $dt = Carbon::create($date[0], $date[1], $date[2], 0, 0, 0);
+            /* 今日の日付だった場合のみビューに渡す */
+            if ($dt->eq(Carbon::today())) {
+                return view('attendances/create');
+            } else {
+            /* 勤怠表示画面に戻す */
+                return redirect()->route('attendances.index');
+            }
+        } else {
+            return redirect()->route('attendances.index');
+        }
+    }
 }
