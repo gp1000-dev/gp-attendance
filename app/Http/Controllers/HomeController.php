de307651f9ccc8406511b8afb0abce73005bca86
gpnakai <nakai@gp1000.jp>
2022-06-24 17:09:14 +0900

    ホーム画面にHelloを表示し、ユーザー情報と勤怠用法のリンクを追加した

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user_id = Auth::user()->id;
+        $user = User::find($user_id);
+
+        return view('home', ['user' => $user]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <?php
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+             ?>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+            <h2>{{$user->last_name}} {{$user->first_name}} さん こんにちは！</h2>
+            
+            <ul>
+                <li><a href="/user">ユーザー情報</a></li>
+                <li><a href="/attendances">勤怠情報</a></li>
+            </ul>
+
         </div>
     </div>
 </div>
