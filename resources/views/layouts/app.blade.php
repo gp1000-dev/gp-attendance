a2ecba46555d507f3350dacca5a635f0aefdbce5
gpnakai <nakai@gp1000.jp>
2022-06-27 14:30:03 +0900

    menuに勤怠情報を追加しました。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,9 @@
                                     <a class="dropdown-item" href="{{ url('/user') }}">
                                         ユーザー情報
                                     </a>
-
+                                    <a class="dropdown-item" href="{{ route('attendances.index') }}">
+                                        勤怠情報
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
