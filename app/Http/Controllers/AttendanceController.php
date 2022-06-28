0148cd14e820cd2357b72453e27d5579cd613fcc
gpnakai <nakai@gp1000.jp>
2022-06-28 13:33:24 +0900

    LOG::を追加しました

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -106,6 +106,7 @@ class AttendanceController extends Controller
         ) {
             abort(403);
         }
+        \Log::debug('status=[' . $request->status . ']');
         if ($request->status === '') {
             abort(403);
         }
