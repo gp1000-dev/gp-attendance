f5f2a5a351c1c94ffb4534dc93a8781f96fe70f2
gpnakai <nakai@gp1000.jp>
2022-06-30 17:03:20 +0900

    statusのステージ忘れを修正しました。

--- resources/views/attendances/edit.blade.php
+++ resources/views/attendances/edit.blade.php
@@ -28,7 +28,7 @@
                                     $maxEndTime = config('app.attendance.max_end_time');
                                     $timeDuration = \Carbon\Carbon::create(config('app.attendance.time_duration'));
                                     $timeDurationMinutes = $timeDuration->hour * 60 + $timeDuration->minute;
-                                    $status = $attendance->status === '' ? '' : $attendance->status;
+                                    $status = $attendance->status;
                                     $period = \Carbon\CarbonPeriod::create($minStartTime, $maxEndTime)->minutes($timeDurationMinutes)->toArray();
                                     $start_time = is_null($attendance->start_time) ? \Carbon\Carbon::create($defaultStartTime) : $attendance->start_time;
                                     $end_time = is_null($attendance->end_time) ? \Carbon\Carbon::create($defaultEndTime) : $attendance->end_time;
