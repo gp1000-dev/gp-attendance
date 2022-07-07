9f239a6f2cb956b15a2a40f82610c8ff650b086c
gpnakai <nakai@gp1000.jp>
2022-07-07 16:49:53 +0900

    UserRequestを追加しました。

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'last_name' => 'required',
+            'first_name' => 'required',
+            'last_kana_name' => 'required',
+            'first_kana_name' => 'required',
+            'gender' => 'required',
+            'birthdate' => 'require',
+            'email' => 'unique:users,email|regex:/^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/'
+
+
+
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'last_name' => '姓',
+            'first_name' => '名前',
+            'last_kana_name' => '姓カナ',
+            'first_kana_name' => '名前カナ',
+
+            'email' => 'email',
+
+        ];
+    }
+}
